9ddcb349eeb37eecd7bac981311bdc80daf7eb20
jayax <rahmatwijaya21866@gmail.com>
2024-10-30 19:25:30 +0800

    first commit

--- resources/views/mahasiswa/tambah.blade.php
+++ resources/views/mahasiswa/tambah.blade.php
@@ -30,5 +30,12 @@
             </div>
         </div>
 
+        <script script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.bundle.min.js" crossorigin="anonymous"></script>
+        <script src="{{ asset('js/scripts.js') }}"></script>
+        <script src="https://cdn.jsdelivr.net/npm/simple-datatables@7.1.2/dist/umd/simple-datatables.min.js" crossorigin="anonymous"></script>
+        <script src="js/datatables-simple-demo.js"></script>
+        <link href="https://cdn.jsdelivr.net/npm/simple-datatables@7.1.2/dist/style.min.css" rel="stylesheet" />
+        <link href="{{ asset('css/styles.css') }}" rel="stylesheet" />
+        <script src="https://use.fontawesome.com/releases/v6.3.0/js/all.js" crossorigin="anonymous"></script>
 
 @endsection
